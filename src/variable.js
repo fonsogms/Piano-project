8cda202dd336a3b170042ddc0ce29332923f8848
Alfonso García-Miñaur <Alfonso@Alfonso-GMS.local>
2019-08-30 10:31:08 +0200

    sin onclick

--- src/variable.js
+++ src/variable.js
@@ -1,24 +1,4 @@
-window.addEventListener("load", () => {
-    const sounds = document.querySelectorAll(".sound");
-    const pads = document.querySelectorAll(".pads div");
-    const visual = document.querySelector(".visual");
-    const colors = [
-      "#60d394",
-      "#d36060",
-      "#c060d3",
-      "#d3d160",
-      "#606bd3",
-      "#60c2d3"
-    ];
 
-    pads.forEach((pad, index) => {
-        pad.addEventListener("click", function() {
-          sounds[index].currentTime = 0;
-          sounds[index].play();
-        });
-      });
-    
-  });
 
 const keys=[49,50,51,52,53,54,55,56,57,48,222,187];
 let randomSequence=[];
